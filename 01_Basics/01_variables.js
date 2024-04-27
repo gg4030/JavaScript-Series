acb5b7c64e4312dc6b44ecbd40742252742e7482
Golagabathula Mohit Muthyala Rao <104147860+gg4030@users.noreply.github.com>
2024-04-27 08:57:49 +0000

    01 Done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountNumber = 1234
+let accountEmail = "123@gmail.com"
+var accountPassword = "123"
+accountCity = "Goa"
+
+// accountNumber = 2
+accountEmail = "abc@gmail.com"
+accountPassword = "abc"
+accountCity = "Hyd"
+
+console.log(accountNumber);
+console.table([accountEmail,accountNumber,accountPassword,accountCity]);
